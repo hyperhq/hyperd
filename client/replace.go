cb4da58972be75ccbee8d846666c5b264148ce9f
Lei Xue <carmark.dlut@gmail.com>
2015-08-11 16:05:13 +0800

    different os have different command, modify help output :trollface:

--- client/replace.go
+++ client/replace.go
@@ -1,3 +1,5 @@
+// +build linux
+
 package client
 
 import (
@@ -23,7 +25,7 @@ func (cli *HyperClient) HyperCmdReplace(args ...string) error {
 		Yaml    bool   `short:"y" long:"yaml" default:"false" default-mask:"-" description:"The Pod file is based on Yaml file"`
 	}
 	var parser = gflag.NewParser(&opts, gflag.Default)
-	parser.Usage = "replace --oldpod POD_ID --newpod POD_ID [--file POD_FILE]\n\nreplace the pod in a running VM with a new one"
+	parser.Usage = "replace --oldpod POD_ID --newpod POD_ID [--file POD_FILE]\n\nReplace the pod in a running VM with a new one"
 	args, err := parser.Parse()
 	if err != nil {
 		if !strings.Contains(err.Error(), "Usage") {
