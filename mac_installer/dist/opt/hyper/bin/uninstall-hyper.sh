aaf61dbd42a9e06cb19b650af3a89419dce6f4f6
Wang Xu <gnawux@gmail.com>
2015-09-15 17:02:28 +0800

    update the mac installer scripts
    - update the pre-install script to correct the vm control commands
    - update the uninstall script to stop and clean puller script,
      and update dirs to be cleaned
    
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- mac_installer/dist/opt/hyper/bin/uninstall-hyper.sh
+++ mac_installer/dist/opt/hyper/bin/uninstall-hyper.sh
@@ -5,7 +5,8 @@ PATH=/usr/local/bin:/usr/bin:/bin
 HYPER_HOME=/opt/hyper
 HYPER_SOCK=/var/run/hyper.sock
 HYPER_RUNTIME=/var/lib/hyper
-HYPER_VM=${HYPER_RUNTIME}/run
+HYPER_VM=/var/run/hyper
+HYPER_VM_TMP=${HYPER_VM}/vm
 HYPER_LOG=/var/log/hyper
 
 UNLINK="${HYPER_HOME}/bin/hunlink"
@@ -23,7 +24,21 @@ if /bin/launchctl list "sh.hyper.hyper" &> /dev/null; then
 fi
 ${RM} -f /Library/LaunchDaemons/sh.hyper.hyper.plist
 
+# clean puller vm
+if which vboxmanage &> /dev/null ; then
+	if vboxmanage showvminfo hyper-mac-pull-vm &> /dev/null; then
+		vboxmanage controlvm hyper-mac-pull-vm poweroff || true
+		vboxmanage unregistervm hyper-mac-pull-vm --delete || true
+	fi
+fi
+rm -rf ${HYPER_VM_TMP}/hyper-mac-pull-vm
+rm -rf ${HYPER_VM}/hyper-mac-pull-vm
+
 if [ "x$PURGE" != "xFALSE" ] ; then
+
+	if [ -d ${HYPER_VM_TMP} ]; then
+		rm -rf ${HYPER_VM_TMP}
+	fi
 
 	# Remove the hyper vm dir, include volumes. In normal case, these dir
 	# should be empty. But remove carefully in case some were not properly
@@ -43,12 +58,14 @@ if [ "x$PURGE" != "xFALSE" ] ; then
 	fi
 	
 	# Remove the images and vm/container
-	${RM} -rf ${HYPER_RUNTIME}/{containers,graph,linkgraph.db,repositories-vbox,tmp,trust,vbox,hyper.db,vm}
+	${RM} -rf ${HYPER_RUNTIME}/{containers,graph,linkgraph.db,repositories-vbox,run,tmp,trust,vbox,lib,vm,hyper.db}
+	if [ -d ${HYPER_RUNTIME} ]; then
+		${RMDIR} ${HYPER_RUNTIME}
+	fi
 fi
 
 # Remove socket, logs, and binaries
 ${RM} -f  ${HYPER_SOCK}
 ${RM} -rf ${HYPER_LOG}
 ${RM} -rf ${HYPER_HOME}
-
 
