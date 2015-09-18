92fb78cdbaa7fb2faed3b6238221a3107aca29fe
Wang Xu <gnawux@gmail.com>
2015-09-18 16:57:21 +0800

    fix device mapper build error with go 1.5
    code from docker upstream
    
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- lib/docker/pkg/devicemapper/devmapper_wrapper.go
+++ lib/docker/pkg/devicemapper/devmapper_wrapper.go
@@ -38,7 +38,10 @@ static void	log_with_errno_init()
 */
 import "C"
 
-import "unsafe"
+import (
+	"reflect"
+	"unsafe"
+)
 
 type (
 	CDmTask C.struct_dm_task
@@ -184,11 +187,20 @@ func dmTaskGetDepsFct(task *CDmTask) *Deps {
 	if Cdeps == nil {
 		return nil
 	}
+
+	// golang issue: https://github.com/golang/go/issues/11925
+	hdr := reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(uintptr(unsafe.Pointer(Cdeps)) + unsafe.Sizeof(*Cdeps))),
+		Len:  int(Cdeps.count),
+		Cap:  int(Cdeps.count),
+	}
+	devices := *(*[]C.uint64_t)(unsafe.Pointer(&hdr))
+
 	deps := &Deps{
 		Count:  uint32(Cdeps.count),
 		Filler: uint32(Cdeps.filler),
 	}
-	for _, device := range Cdeps.device {
+	for _, device := range devices {
 		deps.Device = append(deps.Device, (uint64)(device))
 	}
 	return deps
