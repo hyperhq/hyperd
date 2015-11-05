95c7b466d650efaa6270cd91684511ca12c198b2
Lei Xue <carmark.dlut@gmail.com>
2015-11-05 12:17:43 +0800

    format the output of 'images' and 'list'

--- client/images.go
+++ client/images.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"text/tabwriter"
 	"time"
 
 	"github.com/hyperhq/hyper/engine"
@@ -58,19 +59,21 @@ func (cli *HyperClient) HyperCmdImages(args ...string) error {
 	)
 	imagesList = remoteInfo.GetList("imagesList")
 
+	w := tabwriter.NewWriter(cli.out, 20, 1, 3, ' ', 0)
 	if opts.Num == false {
-		fmt.Printf("%-15s%-20s%-22s%20s     %20s\n", "REPOSITORY", "TAG", "IMAGE ID", "CREATED", "VIRTUAL SIZE")
+		fmt.Fprintln(w, "REPOSITORY\tTAG\tIMAGE ID\tCREATED\tVIRTUAL SIZE")
 		for _, item := range imagesList {
 			fields := strings.Split(item, ":")
 			date, _ := strconv.ParseUint(fields[3], 0, 64)
-			fmt.Printf("%-15s%-20s%-22s%25s%20s\n", fields[0], fields[1], fields[2][:12], time.Unix(int64(date), 0).Format("2006-01-02 15:04:05"), getImageSizeString(fields[4]))
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", fields[0], fields[1], fields[2][:12], time.Unix(int64(date), 0).Format("2006-01-02 15:04:05"), getImageSizeString(fields[4]))
 		}
 	} else {
 		for _, item := range imagesList {
 			fields := strings.Split(item, ":")
-			fmt.Printf("%s\n", fields[2][:12])
+			fmt.Fprintf(w, "%s\n", fields[2][:12])
 		}
 	}
+	w.Flush()
 
 	return nil
 }
