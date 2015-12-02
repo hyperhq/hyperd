7b1471fa5e1d39a5ded877a4d863ed5fc81f0902
Wang Xu <gnawux@gmail.com>
2015-12-02 11:35:15 +0800

    build hyperstart into rpm
    and set as one of dependencies of hyper
    
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- package/centos/make-rpm.sh
+++ package/centos/make-rpm.sh
@@ -14,7 +14,15 @@ cd $PROJECT
 git archive --format=tar.gz master > ${CENTOS_DIR}/rpm/SOURCES/hyper-${VERSION}.tar.gz
 cd $PROJECT/../runv
 git archive --format=tar.gz master > ${CENTOS_DIR}/rpm/SOURCES/runv-${VERSION}.tar.gz
+cd $PROJECT/../hyperstart
+git archive --format=tar.gz master > ${CENTOS_DIR}/rpm/SOURCES/hyperstart-${VERSION}.tar.gz
+cd ..
+git clone https://github.com/bonzini/qboot.git
+cd qboot
+git archive --format=tar.gz master > ${CENTOS_DIR}/rpm/SOURCES/qboot.tar.gz
+
 
 sed -e "s#%PROJECT_ROOT%#${PROJECT}#g" ${CENTOS_DIR}/centos-rpm.pod.in > ${CENTOS_DIR}/centos-rpm.pod
 sed -e "s#%VERSION%#${VERSION}#g" ${CENTOS_DIR}/rpm/SPECS/hyper.spec.in > ${CENTOS_DIR}/rpm/SPECS/hyper.spec
+sed -e "s#%VERSION%#${VERSION}#g" ${CENTOS_DIR}/rpm/SPECS/hyperstart.spec.in > ${CENTOS_DIR}/rpm/SPECS/hyperstart.spec
 
