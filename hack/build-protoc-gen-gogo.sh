5c9f735b019d6ef4f6875229d7e0293abc4f0b76
Pengfei Ni <feiskyer@gmail.com>
2016-06-22 15:20:17 +0800

    Update protobuf by protoc-gen-gogo

--- hack/build-protoc-gen-gogo.sh
+++ hack/build-protoc-gen-gogo.sh
@@ -12,6 +12,6 @@ fi
 HYPER_ROOT=$(dirname "${BASH_SOURCE}")/..
 HYPER_ROOT_ABS=$(cd ${HYPER_ROOT}; pwd)
 export GOPATH=${HYPER_ROOT_ABS}/Godeps/_workspace:$GOPATH
-cd ${HYPER_ROOT}/Godeps/_workspace/src/github.com/golang/protobuf/protoc-gen-go/
+cd ${HYPER_ROOT_ABS}/cmds/protoc-gen-gogo
 go build
 
