06a48c6783608c489fde3cbb08d94bdaf8f18124
Gao feng <omarapazanadi@gmail.com>
2016-08-08 14:14:04 +0800

    check if pod is running before kill
    the vm of pod may be nil
    
    Signed-off-by: Gao feng <omarapazanadi@gmail.com>

--- daemon/kill.go
+++ daemon/kill.go
@@ -5,6 +5,7 @@ import (
 	"syscall"
 
 	"github.com/golang/glog"
+	"github.com/hyperhq/runv/hypervisor/types"
 )
 
 func (daemon *Daemon) KillContainer(name string, sig int64) error {
@@ -15,6 +16,10 @@ func (daemon *Daemon) KillContainer(name string, sig int64) error {
 
 	container := p.PodStatus.Containers[idx].Id
 	glog.V(1).Infof("found container %s to kill, signal %d", container, sig)
+
+	if p.PodStatus.Status != types.S_POD_RUNNING || p.VM == nil {
+		return fmt.Errorf("container %s is stopped\n", container)
+	}
 
 	return p.VM.KillContainer(container, syscall.Signal(sig))
 }
@@ -31,6 +36,10 @@ func (daemon *Daemon) KillPodContainers(podName, container string, sig int64) er
 	for i := range p.PodStatus.Containers {
 		if all || p.PodStatus.Containers[i].Id == container {
 			glog.V(1).Infof("send signal %d to container %s", sig, container)
+			if p.PodStatus.Status != types.S_POD_RUNNING || p.VM == nil {
+				return fmt.Errorf("container %s is stopped\n", container)
+			}
+
 			e := p.VM.KillContainer(p.PodStatus.Containers[i].Id, syscall.Signal(sig))
 			if e != nil {
 				err = e
