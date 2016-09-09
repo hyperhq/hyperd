3e7cfed56378fcf4ebab6ed7d4eead92af3ecb06
Xianglin Gao <xlgao@zju.edu.cn>
2016-09-09 16:13:34 +0800

    add files to create deb package
    Signed-off-by: Xianglin Gao <xlgao@zju.edu.cn>

---
+++ package/ubuntu/make-deb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PROJECT=$(readlink -f $(dirname $0)/../..)
+UBUNTU_DIR=${PROJECT}/package/ubuntu
+VERSION=0.6.2
+
+if [ $# -gt 0 ] ; then
+    VERSION=$1
+fi
+
+# get hyperd tar ball
+cd $PROJECT
+git archive --format=tar.gz master > ${UBUNTU_DIR}/hypercontainer-${VERSION}.tar.gz
+
+# prepair to create source pkg
+mkdir -p ${UBUNTU_DIR}/hypercontainer-${VERSION}
+cd ${UBUNTU_DIR}
+tar -zxvf hypercontainer-${VERSION}.tar.gz -C ${UBUNTU_DIR}/hypercontainer-${VERSION}
+
+# in order to use debian/* to create deb, so put them in the hypercontainer.
+cp -a ${UBUNTU_DIR}/debian ${UBUNTU_DIR}/hypercontainer-${VERSION}
+
+# run dh_make
+cd ${UBUNTU_DIR}/hypercontainer-${VERSION}
+dh_make -s -y -f ../hypercontainer-${VERSION}.tar.gz
+
+# run dpkg-buildpackage
+dpkg-buildpackage -us -uc
+
+#clean up intermediate files
+rm -rf ${UBUNTU_DIR}/hypercontainer-${VERSION}
+
