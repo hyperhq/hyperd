3ba9d64538b47adc1af45ddbfd2e675de52993f2
Crazykev <crazykev@zju.edu.cn>
2017-03-01 10:33:16 +0800

    fix return code comparison in stop client
    Signed-off-by: Crazykev <crazykev@zju.edu.cn>

--- client/stop.go
+++ client/stop.go
@@ -40,7 +40,7 @@ func (cli *HyperClient) HyperCmdStop(args ...string) error {
 				fmt.Fprintf(cli.err, "fail to stop pod %s: %v", args[i], err)
 				continue
 			}
-			if code != types.E_VM_SHUTDOWN {
+			if code != 0 {
 				fmt.Fprintf(cli.err, "Error code is %d, cause is %s", code, cause)
 				continue
 			}
