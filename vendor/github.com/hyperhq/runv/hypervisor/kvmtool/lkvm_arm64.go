01b1941645c40897c729d6fedccaf30814386d92
Hui Zhu <teawater@hyper.sh>
2017-11-27 17:45:56 +0800

    Sync runv arm64 kvmtool
    Signed-off-by: Hui Zhu <teawater@hyper.sh>

--- vendor/github.com/hyperhq/runv/hypervisor/kvmtool/lkvm_arm64.go
+++ vendor/github.com/hyperhq/runv/hypervisor/kvmtool/lkvm_arm64.go
@@ -2,6 +2,40 @@
 
 package kvmtool
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+// On ARM platform, we have different gic interrupt controllers.
+// We have to detect the correct gic chip to set parameter for lkvm.
+func getGicInfo() (info string) {
+	gicinfo, err := os.Open("/proc/interrupts")
+	if err != nil {
+		return "unknown"
+	}
+
+	defer gicinfo.Close()
+
+	scanner := bufio.NewScanner(gicinfo)
+	for scanner.Scan() {
+		newline := scanner.Text()
+		list := strings.Fields(newline)
+
+		for _, item := range list {
+			if strings.EqualFold(item, "GICv2") {
+				return "gicv2"
+			} else if strings.EqualFold(item, "GICv3") ||
+				strings.EqualFold(item, "GICv4") {
+				return "gicv3"
+			}
+		}
+	}
+
+	return "unknown"
+}
+
 func arch_arguments() []string {
-	return []string{"--irqchip", "gicv3"}
+	return []string{"--irqchip", getGicInfo()}
 }
