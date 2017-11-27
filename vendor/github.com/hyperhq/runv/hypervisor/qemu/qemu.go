d03dfc4c9dc897b052c42652cb58497af69edc13
Hui Zhu <teawater@hyper.sh>
2017-11-27 17:02:46 +0800

    Sync runv "Add new option "cache" to container and volume"
    Signed-off-by: Hui Zhu <teawater@hyper.sh>

--- vendor/github.com/hyperhq/runv/hypervisor/qemu/qemu.go
+++ vendor/github.com/hyperhq/runv/hypervisor/qemu/qemu.go
@@ -229,6 +229,7 @@ func (qc *QemuContext) AddDisk(ctx *hypervisor.VmContext, sourceType string, blo
 	format := blockInfo.Format
 	id := blockInfo.ScsiId
 	readonly := blockInfo.ReadOnly
+	cache := blockInfo.Cache
 
 	if format == "rbd" {
 		if blockInfo.Options != nil {
@@ -250,7 +251,7 @@ func (qc *QemuContext) AddDisk(ctx *hypervisor.VmContext, sourceType string, blo
 		}
 	}
 
-	newDiskAddSession(ctx, qc, filename, format, id, readonly, result)
+	newDiskAddSession(ctx, qc, filename, format, id, readonly, cache, result)
 }
 
 func (qc *QemuContext) RemoveDisk(ctx *hypervisor.VmContext, blockInfo *hypervisor.DiskDescriptor, callback hypervisor.VmEvent, result chan<- hypervisor.VmEvent) {
